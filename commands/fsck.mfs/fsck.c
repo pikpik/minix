1f0112dcdcff097461d7fa5e0d99f6d2145ba518
pikpik <pikpik.1010@gmail.com>
2012-09-26 01:20:08 -0300

    Update commands/fsck.mfs/fsck.c

--- commands/fsck.mfs/fsck.c
+++ commands/fsck.mfs/fsck.c
@@ -98,7 +98,7 @@ static struct super_block sb;
  * btoa64 gives the byte address of a block
  */
 #define ztob(z)		((block_nr) (z) << sb.s_log_zone_size)
-#define btoa64(b)	((u64_t)b * block_size)
+#define btoa64(b)	(b * block_size)
 #define SCALE		((int) ztob(1))	/* # blocks in a zone */
 #define FIRST		((zone_nr) sb.s_firstdatazone)	/* as the name says */
 
@@ -657,8 +657,7 @@ void chksuper()
 
 int inoblock(int inn)
 {
-  return (unsigned long)(((u64_t)(inn - 1) * INODE_SIZE) / block_size)
-		+ BLK_ILIST;
+  return (((inn - 1) * INODE_SIZE) / block_size) + BLK_ILIST;
 }
 
 int inooff(int inn)
